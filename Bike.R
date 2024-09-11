b059a528a148c08bbc1d30a7cdc4d7b985cbdd82
Brinley Ostler <brinley.ostler@gmail.com>
2024-09-11 12:55:51 -0600

    Linear Regression Model

--- Bike.R
+++ Bike.R
@@ -3,11 +3,13 @@ library(tidymodels)
 library(patchwork)
 library(vroom)
 
-# read in train dataset with vroom()
+#### READ IN DATA ####
+# read in train & test datasets with vroom()
 train = vroom(file = "train.csv")
+test = vroom(file = "test.csv")
 
 
-# perform an EDA
+#### EXPLORATORY DATA ANALYSIS ####
 glimpse(train)
 
 # Scatterplot of count over time
@@ -32,6 +34,32 @@ humid_plot = ggplot(data = train, mapping = aes(x = humidity, y = count)) +
 
 # create a 4 panel ggplot (patchwork) showing 4 key features of the dataset
   # one panel must be a barplot of weather
-
 (temp_plot + time_plot) / (humid_plot + weather_plot)
+
+
+#### LINEAR REGRESSION ####
+## Set Up and Fit the Linear Regression Model
+my_linear_model <- linear_reg() %>%
+  set_engine("lm") %>% 
+  set_mode("regression") %>% 
+  fit(formula=log(count)~weather+temp+humidity+windspeed, data=train)
+my_linear_model
+
+
+## Generate Predictions Using LInear Model
+bike_predictions <- predict(my_linear_model,
+                            new_data=test)  # Use fit to predict
+bike_predictions
+
+## Format the Predictions for Submission to Kaggle
+kaggle_submission <- bike_predictions %>% 
+  bind_cols(., test) %>% 
+  select(datetime, .pred) %>% 
+  rename(count=.pred) %>% 
+  mutate(count=exp(count)) %>% 
+  mutate(count=pmax(0, count)) %>% 
+  mutate(datetime=as.character(format(datetime)))
+
+## Write out the file
+vroom_write(x=kaggle_submission, file="./LinearPreds.csv", delim=",")
 
